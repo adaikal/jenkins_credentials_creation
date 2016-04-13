818df2cdff5847b5275f7e50d1119d68d925ac43
adaikal <adaikal.raj@gmail.com>
2016-04-13 14:50:07 +0530

    added uuid creation

--- jenkins_credentials_creation.py
+++ jenkins_credentials_creation.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
 import requests
+import uuid
 
+random_uuid = uuid.uuid4()
+print random_uuid
 url = "http://192.168.54.214:8080/credential-store/domain/_/createCredentials"
 
 payload = '''json={
         "": "0", 
         "credentials": {
             "scope": "GLOBAL", 
-            "id": "", 
+            "id": "%s", 
             "username": "adaikal", 
             "password": "xxxyyyzzz", 
             "description": "biz", 
             "$class": "com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl"
         }
-    }'''
+    }''' % random_uuid
 headers = {
     'content-type': "application/x-www-form-urlencoded",
     }
